f10a183c3173baf0dbc7d4e56fc355ae29880a74
Kumho Jeong <nashorn74@gmail.com>
2020-05-16 20:03:16 +0200

    Initial Commit

---
+++ SimpleMusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleMusicApp
+//
+//  Created by Kumho Jeong on 16/05/2020.
+//  Copyright © 2020 OpenMindWorld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
